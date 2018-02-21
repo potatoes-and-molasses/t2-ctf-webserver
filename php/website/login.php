86937354d7c5b0627b0d999c0de697980b0a0705
rbnl <ruben.lumbroso@gmail.com>
2018-02-21 09:31:56 +0000

    fix

--- php/website/login.php
+++ php/website/login.php
@@ -1,9 +1,10 @@
 <?php
-include("config.php");
+include("website/config.php");
 
 if ( isset($_POST["username"]) && isset($_POST["password"]) ){
     if ($_POST["username"]==$username && $_POST["password"]==$password){
-	/* THIS IS NOT A PART OF THE GAME --->*/file_put_contents("log", "logged"); /* <--------- */	
+	/* THIS IS NOT A PART OF THE GAME --->*/$team = $_SERVER['TEAM_NAME'];$url = "http://ec2-35-176-150-168.eu-west-2.compute.amazonaws.com:3000/ctf/finished";$data = array("team"=>$team , "stage"=>"bypassedLogin");$options = array('http' => array('header'  => "api-key: HakunaMatata69\r\n",'method'  => 'POST','content' => http_build_query($data)));$context  = stream_context_create($options);$result = file_get_contents($url, false, $context); /* <--------- */
+	
       print("<h2>Welcome back Master !</h2>");
       print("<br>SSH connection is OK<br/>");
     } else {
